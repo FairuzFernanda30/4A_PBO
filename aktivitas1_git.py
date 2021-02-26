5cda2ba2f0cb5acb97aad129563238fb342297aa
FairuzFernanda30 <fairuzfernanda30@gmail.com>
2021-02-26 09:17:02 +0700

    Aktivitas 1

---
+++ aktivitas1_git.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 09:03:40 2021
+
+@author: Kichiro
+"""
+
+#input
+
+nama=input("Fairuz Fernanda Hermawan")
+umur=20
+
+#output
+print("")
+print("====INPUT DAN OUTPUT PADA BAHASA PEMOGRAMAN====")
+print("nama anda ",nama)
+print("umur anda ",umur)
+printf("")
