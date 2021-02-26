26ab60330181a69d004c1e83072ba33d3dd6a8e7
FairuzFernanda30 <fairuzfernanda30@gmail.com>
2021-02-26 10:06:07 +0700

    aktivitas 4

---
+++ aktivitas4_git.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 10:01:42 2021
+
+@author: Kichiro
+"""
+
+print("perulangan suatu string")
+for huruf in "SISTEL":
+    print(huruf)
+
+nilai_awal=0
+nilai_akhir=25 #tujuan/berapa kali
+step=5
+print("contoh perulangan angka dengan range")
+for i in range(nilai_awal, nilai_akhir, step) :
+    print(i)
