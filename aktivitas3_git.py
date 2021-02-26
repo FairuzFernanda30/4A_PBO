1a1f93565e3a3da542a07048cbeedeb3a54e071a
FairuzFernanda30 <fairuzfernanda30@gmail.com>
2021-02-26 09:47:42 +0700

    aktivitas 3

---
+++ aktivitas3_git.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 09:42:11 2021
+
+@author: Kichiro
+"""
+
+#percabangan
+print("percabangan pada bahasa pemograman python")
+print("pilih bahasa pemograman : ")
+print("1. python")
+print("2. C")
+
+pilih=int(input("pilih menu (1/2) : "))
+
+if pilih == 1:
+    print("bahasa pemograman yang dipilih adalah python")
+elif pilih ==2:
+    print("bahasa pemograman yang dipilih adalah C")
+else :
+    print("pilihan anda tidak tersedia!!")
